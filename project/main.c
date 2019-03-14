e08a33f2e7db2b69e036f019115b8dffce4f6827
LeszekBlazewski <blazewski.leszek@gmail.com>
2019-03-14 14:14:30 +0100

    added tests for new functions

--- project/main.c
+++ project/main.c
@@ -2,21 +2,19 @@
 #include "parser.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
     printf("Enter a number with any precision in system with one of the following bases\n");
     printf("1.Signed binary(insert - at the beginning if the value is negative)\n2.Decimal\n3.Hexadecimal\n4.Octal\n");
     char *number = getNumberFromConsole();
     printf("Number got: %s\n", number);
-    char *thing = hexToBin(number);
-    //char *binaryRep = convertOctalToBinary(number);
-    //char *complementRep = convertBinaryToTwosComplement(number);
-    //printf("Two's complement of number %s is: %s\n", number, complementRep);
-    //free(complementRep);
-    //printf("Binary representation of given number is: %s\n", binaryRep);
-    //char *complementRep = convertBinaryToTwosComplement(binaryRep);
-    //printf("Two's Complement of number %s is %s\n", number, complementRep);
-    //free(binaryRep);
-    //free(number);
-    //free(complementRep);
+    unsigned char *thing = hexToBin(number);
+    printNumber(thing);
+    onesComplement(thing);
+    printNumber(thing);
+    increment(thing);
+    printNumber(thing);
+    free(number);
+    free(thing);
 }
