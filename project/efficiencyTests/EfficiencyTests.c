2cd4ddf12adc054d03662bd4273196eab4ef2976
LeszekBlazewski <blazewski.leszek@gmail.com>
2019-04-25 01:07:30 +0200

    Updated efficiency tests and fixed int overflow in Artithemtic operations.c

--- project/efficiencyTests/EfficiencyTests.c
+++ project/efficiencyTests/EfficiencyTests.c
@@ -5,12 +5,17 @@
 
 void testAdditionAlgoAsm(unsigned char *addend1, unsigned char *addend2)
 {
-    array_adc(addend1, addend2, 1024 * 1024 * 1024 + 1);
+    array_adc(addend1, addend2, 1024 * 1024 * 500 + 1);
+}
+
+void testSubstractionAlgoAsm(unsigned char *minuend, unsigned char *subtrahend)
+{
+    array_sbb(minuend, subtrahend, 1024 * 1024 * 500 + 1);
 }
 
 void testAdditionAlgoC(unsigned char *addend1, unsigned char *addend2, unsigned char *result)
 {
-    unsigned int resultSize = 1024 * 1024 * 1024 + 1;
+    unsigned int resultSize = 1024 * 1024 * 500 + 1;
     unsigned char carry = 0;
     for (int i = resultSize - 1; i >= 0; i--)
     {
@@ -25,41 +30,87 @@ void testAdditionAlgoC(unsigned char *addend1, unsigned char *addend2, unsigned 
 int main()
 {
     // prepare data
-    unsigned char *addend1GB = getNumberFromBinaryFile("./data/addend1GB");
-    unsigned char *addend2GB = getNumberFromBinaryFile("./data/addend2GB");
-    unsigned char *addend1MB = getNumberFromBinaryFile("./data/addend1MB");
-    unsigned char *addend2MB = getNumberFromBinaryFile("./data/addend2MB");
+    //unsigned char *firstNumber1GB = getNumberFromBinaryFile("./data/firstNumber1GB");
+    //unsigned char *secondNumber1GB = getNumberFromBinaryFile("./data/secondNumber1GB");
+    unsigned char *firstNumber500MB = getNumberFromBinaryFile("./data/firstNumber500MB");
+    unsigned char *secondNumber500MB = getNumberFromBinaryFile("./data/secondNumber500MB");
 
-    TCNumber *addend1MBTC = createTCNumber(addend1MB, 1024 * 1024 * 100, 0);
-    TCNumber *addend2MBTC = createTCNumber(addend2MB, 1024 * 1024 * 100, 0);
+    TCNumber *addend1MBTC = createTCNumber_no_realloc(firstNumber500MB, 1024 * 1024 * 500, 0);
+    TCNumber *addend2MBTC = createTCNumber_no_realloc(secondNumber500MB, 1024 * 1024 * 500, 0);
 
     // Test assembly addition on 1 GB numbers
     clock_t t = clock();
-    testAdditionAlgoAsm(addend1GB, addend2GB);
+    testAdditionAlgoAsm(firstNumber500MB, secondNumber500MB);
     t = clock() - t;
     double time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
-    printf("Array addition operation on 1GB numbers in assembly took %f seconds to execute \n", time_taken);
+    printf("Array addition operation on 500MB numbers in assembly took %f seconds to execute \n", time_taken);
 
     // Test C addition on 1 GB numbers
-    unsigned char *result = calloc(1024 * 1024 * 1024 + 1, sizeof(char));
+    unsigned char *resultAdd = calloc(1024 * 1024 * 500 + 1, sizeof(char));
     t = clock();
-    testAdditionAlgoC(addend1GB, addend2GB, result);
+    testAdditionAlgoC(firstNumber500MB, secondNumber500MB, resultAdd);
     t = clock() - t;
     time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
-    printf("Array addition operation on 1GB numbers in C took %f seconds to execute \n", time_taken);
+    printf("Array addition operation on 500MB numbers in C took %f seconds to execute \n", time_taken);
+    free(resultAdd);
 
-    // Test whole addition function which uses assembly inside on 100 MB numbers
+    // Test whole addition function which uses assembly inside on 500 MB numbers
     t = clock();
-    add_asm(addend1MBTC, addend2MBTC);
+    TCNumber *temp = add_asm(addend1MBTC, addend2MBTC);
     t = clock() - t;
     time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
-    printf("Addition operation in assembly on 100MB numbers took %f seconds to execute \n", time_taken);
+    printf("Addition operation in assembly on 500MB numbers took %f seconds to execute \n", time_taken);
+    delete (temp);
 
-    // Test whole addition function which uses assembly inside on 100 MB numbers
+    // Recreate files
+    firstNumber500MB = getNumberFromBinaryFile("./data/firstNumber500MB");
+    secondNumber500MB = getNumberFromBinaryFile("./data/secondNumber500MB");
+
+    addend1MBTC = createTCNumber_no_realloc(firstNumber500MB, 1024 * 1024 * 500, 0);
+    addend2MBTC = createTCNumber_no_realloc(secondNumber500MB, 1024 * 1024 * 500, 0);
+
+    // Test whole addition function which uses C inside on 500 MB numbers
     t = clock();
-    add(addend1MBTC, addend2MBTC);
+    TCNumber *temp1 = add(addend1MBTC, addend2MBTC);
     t = clock() - t;
     time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
-    printf("Addition operation in C on 100MB numbers took %f seconds to execute \n", time_taken);
+    printf("Addition operation in C on 500MB numbers took %f seconds to execute \n", time_taken);
+    free(temp1);
+
+    // Recreate files
+    firstNumber500MB = getNumberFromBinaryFile("./data/firstNumber500MB");
+    secondNumber500MB = getNumberFromBinaryFile("./data/secondNumber500MB");
+
+    addend1MBTC = createTCNumber_no_realloc(firstNumber500MB, 1024 * 1024 * 500, 0);
+    addend2MBTC = createTCNumber_no_realloc(secondNumber500MB, 1024 * 1024 * 500, 0);
+
+    // Test assembly substraction on 1Gb numbers
+    t = clock();
+    testAdditionAlgoAsm(firstNumber500MB, secondNumber500MB);
+    t = clock() - t;
+    time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
+    printf("Array subtraction operation on 500MB numbers in assembly took %f seconds to execute \n", time_taken);
+
+    // Test whole substraction in C on 1 GB numbers
+    t = clock();
+    subtract(addend1MBTC, addend2MBTC);
+    t = clock() - t;
+    time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
+    printf("Subtraction operation on 500MB numbers in C took %f seconds to execute \n", time_taken);
+
+    // Recreate files
+    firstNumber500MB = getNumberFromBinaryFile("./data/firstNumber500MB");
+    secondNumber500MB = getNumberFromBinaryFile("./data/secondNumber500MB");
+
+    addend1MBTC = createTCNumber_no_realloc(firstNumber500MB, 1024 * 1024 * 500, 0);
+    addend2MBTC = createTCNumber_no_realloc(secondNumber500MB, 1024 * 1024 * 500, 0);
+
+    // Test whole substraction which uses assembly on 1 GB numbers
+    t = clock();
+    subtract_asm(addend1MBTC, addend2MBTC);
+    t = clock() - t;
+    time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
+    printf("Subtraction operation in assembly on 500MB numbers took %f seconds to execute \n", time_taken);
+
     return 0;
 }
--- project/src/ArithmeticOperations.c
+++ project/src/ArithmeticOperations.c
@@ -2,41 +2,39 @@
 
 TCNumber *add_asm(TCNumber *addend1, TCNumber *addend2)
 {
-    int highestPos = addend1->numberPosition + (int)addend1->numberSize * 8;
-    if (addend2->numberPosition + (int)addend2->numberSize * 8 > highestPos)
-        highestPos = addend2->numberPosition + (int)addend2->numberSize * 8;
+    long long highestPos = addend1->numberPosition + (long long)addend1->numberSize * 8;
+    if (addend2->numberPosition + (long long)addend2->numberSize * 8 > highestPos)
+        highestPos = addend2->numberPosition + (long long)addend2->numberSize * 8;
     int lowestPos = addend1->numberPosition;
     if (addend2->numberPosition < lowestPos)
         lowestPos = addend2->numberPosition;
     unsigned int resultSize = (highestPos - lowestPos) / 8 + 1;
 
     TCNumber *scaledAddend1 = scaleNumber(addend1, resultSize, lowestPos);
-    delete(addend1);
+    delete (addend1);
     TCNumber *scaledAddend2 = scaleNumber(addend2, resultSize, lowestPos);
-    delete(addend2);
+    delete (addend2);
 
     array_adc(scaledAddend1->number, scaledAddend2->number, resultSize);
 
-    TCNumber *finalResult = createTCNumber_no_realloc(scaledAddend1->number, resultSize, lowestPos);
-    free(scaledAddend1);
-    delete(scaledAddend2);
-    return finalResult;
+    delete (scaledAddend2);
+    return scaledAddend1;
 }
 
 TCNumber *add(TCNumber *addend1, TCNumber *addend2)
 {
-    int highestPos = addend1->numberPosition + (int)addend1->numberSize * 8;
+    long long highestPos = addend1->numberPosition + (long long)addend1->numberSize * 8;
     if (addend2->numberPosition + (int)addend2->numberSize * 8 > highestPos)
-        highestPos = addend2->numberPosition + (int)addend2->numberSize * 8;
+        highestPos = addend2->numberPosition + (long long)addend2->numberSize * 8;
     int lowestPos = addend1->numberPosition;
     if (addend2->numberPosition < lowestPos)
         lowestPos = addend2->numberPosition;
     unsigned int resultSize = (highestPos - lowestPos) / 8 + 1;
 
     TCNumber *scaledAddend1 = scaleNumber(addend1, resultSize, lowestPos);
-    delete(addend1);
+    delete (addend1);
     TCNumber *scaledAddend2 = scaleNumber(addend2, resultSize, lowestPos);
-    delete(addend2);
+    delete (addend2);
 
     unsigned char *result = calloc(resultSize, sizeof(char));
 
@@ -50,8 +48,8 @@ TCNumber *add(TCNumber *addend1, TCNumber *addend2)
             carry = 0;
     }
 
-    delete(scaledAddend1);
-    delete(scaledAddend2);
+    delete (scaledAddend1);
+    delete (scaledAddend2);
     TCNumber *finalResult = createTCNumber_no_realloc(result, resultSize, lowestPos);
     return finalResult;
 }
@@ -63,30 +61,26 @@ TCNumber *subtract(TCNumber *minuend, TCNumber *subtrahend)
     onesComplement(negated);
     increment(negated);
     TCNumber *result = add(minuend, negated);
-    delete(minuend);
-    delete(negated);
     return result;
 }
 
 TCNumber *subtract_asm(TCNumber *minuend, TCNumber *subtrahend)
 {
-    int highestPos = minuend->numberPosition + (int)minuend->numberSize * 8;
+    long long highestPos = minuend->numberPosition + (long long)minuend->numberSize * 8;
     if (subtrahend->numberPosition + (int)subtrahend->numberSize * 8 > highestPos)
-        highestPos = subtrahend->numberPosition + (int)subtrahend->numberSize * 8;
+        highestPos = subtrahend->numberPosition + (long long)subtrahend->numberSize * 8;
     int lowestPos = minuend->numberPosition;
     if (subtrahend->numberPosition < lowestPos)
         lowestPos = subtrahend->numberPosition;
     unsigned int resultSize = (highestPos - lowestPos) / 8 + 1;
 
     TCNumber *scaledMinuend = scaleNumber(minuend, resultSize, lowestPos);
-    delete(minuend);
+    delete (minuend);
     TCNumber *scaledSubstrahend = scaleNumber(subtrahend, resultSize, lowestPos);
-    delete(subtrahend);
+    delete (subtrahend);
 
     array_sbb(scaledMinuend->number, scaledSubstrahend->number, resultSize);
 
-    TCNumber *finalResult = createTCNumber_no_realloc(scaledMinuend->number, resultSize, lowestPos);
-    free(scaledMinuend);
-    delete(scaledSubstrahend);
-    return finalResult;
+    delete (scaledSubstrahend);
+    return scaledMinuend;
 }
