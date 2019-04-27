63eb126f6c35acb0550f4c29908ad195d07168e1
dex1g <dexopl@gmail.com>
2019-04-27 13:57:29 +0200

    asm subtraction is now much faster too

--- project/src/array_adc.s
+++ project/src/array_adc.s
@@ -6,11 +6,11 @@ array_adc:
   movl 16(%ebp), %edi     # edi stores the length of the given numbers
   clc                     # clear the carry flag
   decl %edi               # decrease given length by one becasue array starts from 0
-  movl 8(%ebp), %ecx      # ecx stores the pointer to the first argument
-  movl 12(%ebp), %edx     # edx holds the pointer to the second argument
+  movl 8(%ebp), %ebx      # ebx stores the pointer to the first argument
+  movl 12(%ebp), %ecx     # ecx holds the pointer to the second argument
 loop:
-  movb (%edx,%edi), %al   # second arg in al
-  adc %al, (%ecx,%edi)    # add and store the calculated value in the first argument
+  movb (%ecx,%edi), %al   # second arg in al
+  adc %al, (%ebx,%edi)    # add and store the calculated value in the first argument
   decl %edi
   jns loop                # check if sign flag is not set
   popl %ebp
--- project/src/array_sbb.s
+++ project/src/array_sbb.s
@@ -6,15 +6,12 @@ array_sbb:
   movl 16(%ebp), %edi     # edi stores the length of the given numbers
   clc                     # clear the carry flag
   decl %edi               # decrease given length by one becasue array starts from 0
-loop:
   movl 8(%ebp), %ebx      # ebx stores the pointer to the first argument
-  movl 12(%ebp), %eax     # eax holds the pointer to the second argument
-  movb (%ebx,%edi), %bl
-  movb (%eax,%edi), %al
-  sbb %al, %bl
-  movl 8(%ebp), %eax      # get again the address of first argument
-  movb %bl, (%eax,%edi)   # store the current calculated value in the first argument
+  movl 12(%ebp), %ecx     # ecx holds the pointer to the second argument
+loop:
+  movb (%ecx,%edi), %al   # second arg in al
+  sbb %al, (%ebx,%edi)    # subtract and store the calculated value in the first argument
   decl %edi
-  jns loop                # chec if sign flat is not set
+  jns loop                # check if sign flag is not set
   popl %ebp
   ret
