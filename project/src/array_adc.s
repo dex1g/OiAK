86326d70050e1684a855b619f1727980ade81fa7
dex1g <dexopl@gmail.com>
2019-04-27 15:01:24 +0200

    asm functions now preserve edi and ebx state

--- project/src/array_adc.s
+++ project/src/array_adc.s
@@ -3,15 +3,17 @@
 array_adc:
   pushl %ebp
   movl %esp, %ebp
+  pushl %edi
   movl 16(%ebp), %edi     # edi stores the length of the given numbers
   clc                     # clear the carry flag
   decl %edi               # decrease given length by one becasue array starts from 0
-  movl 8(%ebp), %ebx      # ebx stores the pointer to the first argument
+  movl 8(%ebp), %edx      # edx stores the pointer to the first argument
   movl 12(%ebp), %ecx     # ecx holds the pointer to the second argument
 loop:
   movb (%ecx,%edi), %al   # second arg in al
-  adc %al, (%ebx,%edi)    # add and store the calculated value in the first argument
+  adc %al, (%edx,%edi)    # add and store the calculated value in the first argument
   decl %edi
   jns loop                # check if sign flag is not set
+  popl %edi
   popl %ebp
   ret
