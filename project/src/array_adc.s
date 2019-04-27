cc072927232dbc52e16b40d5acd999a5f41cf176
dex1g <dexopl@gmail.com>
2019-04-27 13:47:30 +0200

    assembly adding should now be much faster

--- project/src/array_adc.s
+++ project/src/array_adc.s
@@ -6,14 +6,11 @@ array_adc:
   movl 16(%ebp), %edi     # edi stores the length of the given numbers
   clc                     # clear the carry flag
   decl %edi               # decrease given length by one becasue array starts from 0
+  movl 8(%ebp), %ecx      # ecx stores the pointer to the first argument
+  movl 12(%ebp), %edx     # edx holds the pointer to the second argument
 loop:
-  movl 8(%ebp), %ebx      # ebx stores the pointer to the first argument
-  movl 12(%ebp), %eax     # eax holds the pointer to the second argument
-  movb (%ebx,%edi), %bl
-  movb (%eax,%edi), %al
-  adc %al, %bl
-  movl 8(%ebp), %eax      # get again the address of first argument
-  movb %bl, (%eax,%edi)   # store the current calculated value in the first argument
+  movb (%edx,%edi), %al   # second arg in al
+  adc %al, (%ecx,%edi)    # add and store the calculated value in the first argument
   decl %edi
   jns loop                # check if sign flag is not set
   popl %ebp
